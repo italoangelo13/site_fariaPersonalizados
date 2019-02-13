b13fc335f805ed79f17501499f4af07ac82485cc
italoangelo13 <italoangelo.ti@gmail.com>
2019-02-13 12:23:51 -0200

    pronto

--- assets/local/scripts.js
+++ assets/local/scripts.js
@@ -44,3 +44,76 @@ function msg(msg){
         typeAnimated: true,
     });
 }
+
+
+
+
+//Porcelana
+function enviarPorcelana(modal){
+    var nome = $('#nomePorcelana').val();
+    var prod = $('#produtoPorcelana').val();
+    var quant = $('#quantPorcelana').val();
+
+    var msg = 'Olá, meu nome é ' + nome + ', gostaria de fazer um pedido de ' + quant + ' ' + prod + '.';
+    console.log(msg);
+    $(modal).modal('hide')
+    msgSucesso('Pedido Enviado com sucesso.');
+    window.open('https://api.whatsapp.com/send?phone=553195760071&text='+msg,'_blank');
+}
+
+
+//Polimero
+function enviarPolimero(modal){
+    var nome = $('#nomePolimero').val();
+    var prod = $('#produtoPolimero').val();
+    var quant = $('#quantPolimero').val();
+
+    var msg = 'Olá, meu nome é ' + nome + ', gostaria de fazer um pedido de ' + quant + ' ' + prod + '.';
+    console.log(msg);
+    $(modal).modal('hide')
+    msgSucesso('Pedido Enviado com sucesso.');
+    window.open('https://api.whatsapp.com/send?phone=553195760071&text='+msg,'_blank');
+}
+
+
+
+//Acrilico
+function enviarAcrilico(modal){
+    var nome = $('#nomeAcrilico').val();
+    var prod = $('#produtoAcrilico').val();
+    var quant = $('#quantAcrilico').val();
+
+    var msg = 'Olá, meu nome é ' + nome + ', gostaria de fazer um pedido de ' + quant + ' ' + prod + '.';
+    console.log(msg);
+    $(modal).modal('hide')
+    msgSucesso('Pedido Enviado com sucesso.');
+    window.open('https://api.whatsapp.com/send?phone=553195760071&text='+msg,'_blank');
+}
+
+
+//Copo Acrilico
+function enviarCopoAcrilico(modal){
+    var nome = $('#nomeCopoAcrilico').val();
+    var prod = $('#produtoCopoAcrilico').val();
+    var quant = $('#quantCopoAcrilico').val();
+
+    var msg = 'Olá, meu nome é ' + nome + ', gostaria de fazer um pedido de ' + quant + ' ' + prod + '.';
+    console.log(msg);
+    $(modal).modal('hide')
+    msgSucesso('Pedido Enviado com sucesso.');
+    window.open('https://api.whatsapp.com/send?phone=553195760071&text='+msg,'_blank');
+}
+
+
+//Etiquetas
+function enviarEtiqueta(modal){
+    var nome = $('#nomeEtiqueta').val();
+    var prod = $('#produtoEtiqueta').val();
+    var quant = $('#quantEtiqueta').val();
+
+    var msg = 'Olá, meu nome é ' + nome + ', gostaria de fazer um pedido de ' + quant + ' ' + prod + '.';
+    console.log(msg);
+    $(modal).modal('hide')
+    msgSucesso('Pedido Enviado com sucesso.');
+    window.open('https://api.whatsapp.com/send?phone=553195760071&text='+msg,'_blank');
+}
